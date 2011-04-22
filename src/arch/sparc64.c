a932a7fc5410a459d422171d7039ad47dee317e5
Pierre Pronchery <khorben@defora.org>
2011-04-22 17:28:41 +0000

    Fixed the file information

--- src/arch/sparc64.c
+++ src/arch/sparc64.c
@@ -1,6 +1,6 @@
 /* $Id$ */
 /* Copyright (c) 2011 Pierre Pronchery <khorben@defora.org> */
-/* This file is part of DeforaOS Devel as */
+/* This file is part of DeforaOS Devel asm */
 /* This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 3 of the License.

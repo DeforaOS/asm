95e7f4175aa72f5bebd850bfd5605fc33a6557c9
Pierre Pronchery <khorben@defora.org>
2013-10-25 23:05:50 +0200

    Updated the copyright information

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 #$Id$
-#Copyright (c) 2012 Pierre Pronchery <khorben@defora.org>
+#Copyright (c) 2012-2013 Pierre Pronchery <khorben@defora.org>
 #This file is part of DeforaOS Devel Asm
 #This program is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by

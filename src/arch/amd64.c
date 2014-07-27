cd59194de555b242e6d6f414ffdc7a822318028d
Pierre Pronchery <khorben@defora.org>
2014-07-27 14:16:52 +0200

    Updated the copyright line

--- src/arch/amd64.c
+++ src/arch/amd64.c
@@ -1,5 +1,5 @@
 /* $Id$ */
-/* Copyright (c) 2011-2013 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2011-2014 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS Devel asm */
 /* This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by

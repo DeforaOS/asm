5201f5c6be2a88af37c1a33148e403d2394556b8
Pierre Pronchery <khorben@defora.org>
2014-11-16 01:47:17 +0100

    Re-indented the Python script

--- src/python/libasm.py
+++ src/python/libasm.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2.7
 #$Id$
-#Copyright (c) 2013 Pierre Pronchery <khorben@defora.org>
+#Copyright (c) 2013-2014 Pierre Pronchery <khorben@defora.org>
 #This file is part of DeforaOS Devel asm
 #This program is free software: you can redistribute it and/or modify
 #it under the terms of the GNU Lesser General Public License as published by
@@ -21,38 +21,38 @@ import _libasm
 
 #Asm
 class Asm:
-	def __init__(self, arch, format):
-		self.asm = _libasm.asm_new(arch, format)
+    def __init__(self, arch, format):
+        self.asm = _libasm.asm_new(arch, format)
 
-	def get_arch(self):
-		return _libasm.asm_get_arch(self.asm)
+    def get_arch(self):
+        return _libasm.asm_get_arch(self.asm)
 
-	def get_format(self):
-		return _libasm.asm_get_format(self.asm)
+    def get_format(self):
+        return _libasm.asm_get_format(self.asm)
 
-	def set_arch(self, arch):
-		return _libasm.asm_set_arch(self.asm, arch)
+    def set_arch(self, arch):
+        return _libasm.asm_set_arch(self.asm, arch)
 
-	def set_format(self, format):
-		return _libasm.asm_set_format(self.asm, format)
+    def set_format(self, format):
+        return _libasm.asm_set_format(self.asm, format)
 
-	def guess_arch(self):
-		return _libasm.asm_guess_arch(self.asm)
+    def guess_arch(self):
+        return _libasm.asm_guess_arch(self.asm)
 
-	def guess_format(self):
-		return _libasm.asm_guess_format(self.asm)
+    def guess_format(self):
+        return _libasm.asm_guess_format(self.asm)
 
-	def close(self):
-		return _libasm.asm_close(self.asm)
+    def close(self):
+        return _libasm.asm_close(self.asm)
 
-	def assemble_string(self, outfile, string):
-		return _libasm.asm_assemble_string(self.asm, outfile, string)
+    def assemble_string(self, outfile, string):
+        return _libasm.asm_assemble_string(self.asm, outfile, string)
 
-	def open_assemble(self, outfile):
-		return _libasm.asm_open_assemble(self.asm, outfile)
+    def open_assemble(self, outfile):
+        return _libasm.asm_open_assemble(self.asm, outfile)
 
-	def instruction(self, name):
-		return _libasm.asm_instruction(self.asm, name)
+    def instruction(self, name):
+        return _libasm.asm_instruction(self.asm, name)
 
-	def open_deassemble(self, filename, raw):
-		return _libasm.asm_open_deassemble(self.asm, filename, raw)
+    def open_deassemble(self, filename, raw):
+        return _libasm.asm_open_deassemble(self.asm, filename, raw)

1d12cbdc3a1eed3e0ed6310b53d3787c7343e7e0
Pierre Pronchery <khorben@defora.org>
2018-09-09 20:31:21 +0200

    Expect an argument for push instructions

--- tests/eth.asm
+++ tests/eth.asm
@@ -1,7 +1,7 @@
 /* $Id$ */
 .section .text
-	push1	0x01		/* 60 01 */
-	push1	0x00		/* 60 00 */
+	push1	$0x01		/* 60 01 */
+	push1	$0x00		/* 60 00 */
 	dup2			/* 81 */
 	swap1			/* 90 */
 	sstore			/* 55 */
